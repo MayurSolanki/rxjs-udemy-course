f91e5c19b92427dd38f32d2c119b59851724734f
MayurSolanki <mayursolanki120@gmail.com>
2023-04-28 12:25:04 +0530

    filter and map opearator

--- index.ts
+++ index.ts
@@ -144,3 +144,47 @@ of('food')
     complete: () => console.log('Completed')
   });
   
+  // Filter opearator - 
+  interface NewsItem {
+    category: 'Business' | 'Sports';
+    content: string;
+  }
+  
+  const newsFeed$ = new Observable<NewsItem>(subscriber => {
+    setTimeout(() => 
+      subscriber.next({ category: 'Business', content: 'A' }), 1000);
+    setTimeout(() => 
+      subscriber.next({ category: 'Sports', content: 'B' }), 3000);
+    setTimeout(() => 
+      subscriber.next({ category: 'Business', content: 'C' }), 4000);
+    setTimeout(() => 
+      subscriber.next({ category: 'Sports', content: 'D' }), 6000);
+    setTimeout(() => 
+      subscriber.next({ category: 'Business', content: 'E' }), 7000);
+  });
+  
+  const sportsNewsFeed$ = newsFeed$.pipe(
+    filter(item => item.category === 'Sports')
+  );
+  
+  newsFeed$.subscribe(
+    item => console.log(item)
+  );
+
+  // Map operator
+  const randomFirstName$ = ajax<any>('https://random-data-api.com/api/name/random_name').pipe(
+  map(ajaxResponse => ajaxResponse.response.first_name)
+);
+
+const randomCapital$ = ajax<any>('https://random-data-api.com/api/nation/random_nation').pipe(
+  map(ajaxResponse => ajaxResponse.response.capital)
+);
+
+const randomDish$ = ajax<any>('https://random-data-api.com/api/food/random_food').pipe(
+  map(ajaxResponse => ajaxResponse.response.dish)
+);
+
+forkJoin([randomFirstName$, randomCapital$, randomDish$]).subscribe(
+  ([firstName, capital, dish]) =>
+    console.log(`${firstName} is from ${capital} and likes to eat ${dish}.`)
+);
